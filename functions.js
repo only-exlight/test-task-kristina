0b1819ee40278b7bcebf64118db72d8706f1895e
Andrey Kernichnyy <andrey.kernichniy@efly.aero>
2017-08-04 22:37:37 +0600

    Fix and cooment

--- functions.js
+++ functions.js
@@ -1,29 +1,35 @@
-let oneVectorDraw = (ctx, radius, a) => {
+/**
+ * 
+ * @param {Canvas Context} ctx 
+ * @param {Number} a 
+ * @param {Number} margin 
+ * @param {String} color 
+ */
+let vectorDraw = (ctx, a, margin, color) => {
+    const segment = 19.49,
+        startLine = 35, //Начало линейки (подобрано эксперементально)
+        yLine = 195; // Линия линейки по y 
+    let radius = radiusCalculate(a);
+    //Рисуем дугу
     ctx.beginPath();
-    ctx.arc(radius + 35, 195, radius, 0, Math.PI, true);
+    ctx.arc(startLine + margin + radius, yLine, radius, Math.PI, 0); //Аргументы x,y,R,начальный угол, конечный угол
     ctx.lineWidth = 3;
-    ctx.strokeStyle = 'red';
+    ctx.strokeStyle = color; //Цвет контуров
     ctx.stroke();
-
+    //Рисуем триугольник в конечной точке
     ctx.beginPath();
-    ctx.moveTo(19.49 * a * 2 + 35 + 6, 195 - 15);
-    ctx.lineTo(19.49 * a * 2 + 35, 195);
-    ctx.lineTo(19.49 * a * 2 + 35 - 6, 195 - 15);
-    ctx.fillStyle = 'red';
+    ctx.moveTo(radius * 2 + startLine + margin + 6, yLine - 15); //Переместить перо в точку (x,y) x = диаметр + startLine + margin, по y выше на 15
+    ctx.lineTo(radius * 2 + startLine + margin, yLine); //Нарисовать линию из предыдущей точки по диагонили вниз
+    ctx.lineTo(radius * 2 + startLine + margin - 6, yLine - 15); //Нарисовать линию из предыдущей точки по диагонили вверх
+    ctx.fillStyle = color; //Цвет закраски
     ctx.fill();
 }
 
-let twoVectorDraw = (ctx, radius2, radius,a,b) => {
-    ctx.beginPath();
-    ctx.arc(radius2 + radius * 2 + 35, 195, radius2, 0, Math.PI, true);
-    ctx.lineWidth = 3;
-    ctx.strokeStyle = 'orange';
-    ctx.stroke();
-
-    ctx.beginPath();
-    ctx.moveTo(19.49 * a * 2 + 19.49 * b * 2 + 35 + 6, 195 - 15);
-    ctx.lineTo(19.49 * a * 2 + 19.49 * b * 2 + 35, 195);
-    ctx.lineTo(19.49 * a * 2 + 19.49 * b * 2 + 35 - 6, 195 - 15);
-    ctx.fillStyle = 'orange';
-    ctx.fill();
+/**
+ * 
+ * @param {Number} value 
+ */
+let radiusCalculate = (value) => {
+    const segment = 19.49; //Ширина одного деления (подобрано эксперементально)
+    return value * segment;
 }
--- script.js
+++ script.js
@@ -1,76 +1,71 @@
+//События загрузки страницы
 window.onload = ()=> {
-    let doc = document,
-        a_span = doc.getElementById('a'),
-        b_span = doc.getElementById('b'),
-        res_inp = doc.getElementById('result'),
-        inputAblock = doc.getElementById('input-a-block'),
-        inputBblock = doc.getElementById('input-b-block'),
-        ask = doc.getElementById('ask'),
-        inputA = doc.getElementById('input-a'),
-        inputB = doc.getElementById('input-b');
-
-    let a = Math.floor(Math.random() * (9 - 6 + 1)) + 6,
-        result = Math.floor(Math.random() * (14 - 11 + 1)) + 11,
-        b = result - a,
-        radius = 19.49 * a,
-        radius2 = 19.49 * b;
-
-    a_span.innerHTML = a;
-    b_span.innerHTML = b;
-
-    inputAblock.style.left = radius + 12 + 'px';
-    inputBblock.style.left = radius2 + radius *2 -36 + 'px';
-
-    let canvas = doc.getElementById('canvas'),
-        ctx = canvas.getContext("2d"),
+    //Получение элементов документа
+    let aBox = document.getElementById('a'),
+        bBox = document.getElementById('b'),
+        resBox = document.getElementById('res'),
+        inputResult = document.getElementById('result'),
+        inputAblock = document.getElementById('input-a-block'),
+        inputBblock = document.getElementById('input-b-block'),
+        inputA = document.getElementById('input-a'),
+        inputB = document.getElementById('input-b');
+    //Инициализация canvas
+    let canvas = document.getElementById('canvas'),
+        ctx = canvas.getContext("2d"),//Задание и сохранение контекста
         img = new Image();
     img.src = 'sprite.png';
-    canvas.height = '170';
-    canvas.width = '875';
-
-    img.onload = ()=>{
-        ctx.drawImage(img,0,80);
-        ctx.scale(1,0.5);
-        oneVectorDraw(ctx,radius,a);
+    //Математика
+    let a = Math.floor(Math.random() * (9 - 6 + 1)) + 6, //Получаем число а в диапозоне [6-9]
+        result = Math.floor(Math.random() * (14 - 11 + 1)) + 11, //Получаем число b в диапозоне [11-14]
+        b = result - a; //Получаем число b
+    //Вывод в соответсвующие блоки
+    aBox.innerText = a; bBox.innerText = b;
+    //Задание отступов для полей ввода a,b
+    inputAblock.style.left = radiusCalculate(a) + 12 + 'px';
+    inputBblock.style.left = radiusCalculate(b) + radiusCalculate(a) * 2 - 36 + 'px';
+    //Событие окончания загрузки изображения
+    img.onload = () => {
+        ctx.drawImage(img, 0, 80); //Отрисовка изображения
+        ctx.scale(1, 0.5); //Сужение последущих отрисовок по оси y, 0.5 от первоночального
+        vectorDraw(ctx, a, 0, 'purple'); //Рисуем первую стрелку
     }
-
-
-    inputA.onchange = () => {
+    //Событие нажатия поля ввода числа a
+    inputA.onkeyup = () => {
         if (inputA.value != a){
-            inputA.style.color = 'red';
-            a_span.style.background = 'yellow';
+            inputA.style.color = 'brown';
+            aBox.style.background = 'wheat';
         } else {
-            let span = document.createElement("SPAN");
-            span.innerText = a;
-            inputAblock.appendChild(span);
-            inputAblock.removeChild(inputA);
-            a_span.style.background = 'white';
-            twoVectorDraw(ctx,radius2,radius,a,b);
+            let div = document.createElement("div"); //Создание нового div
+            div.innerText = a; //Добавим текст
+            inputAblock.appendChild(div); //Поместим созданный div в блок inputAblock
+            inputAblock.removeChild(inputA); //Удалим в inputAblock input;
+            aBox.style.background = 'white'; //Вернем background в white на случай если цвет менялся 
+            vectorDraw(ctx, b, radiusCalculate(a) * 2,'purple'); //Отрисуем стрелочку 2
         }
     }
-
-    inputB.onchange = () => {
+    //Событие нажатия поля ввода числа b
+    inputB.onkeyup = () => {
         if (inputB.value != b) {
-            inputB.style.color = 'red';
-            b_span.style.background = 'yellow';
+            inputB.style.color = 'firebrick';
+            bBox.style.background = 'wheat';
         } else {
-            let span = document.createElement("SPAN");
-            span.innerText = b;
-            inputBblock.appendChild(span);
+            let div = document.createElement('div');
+            div.innerText = b;
+            inputBblock.appendChild(div);
             inputBblock.removeChild(inputB);
-            a_span.style.background = 'white';
-            ask.style.display = 'none';
-            res_inp.style.display = 'inline-block';
+            bBox.style.background = 'white';
+            resBox.style.display = 'none'; //Скроем блок результата
+            inputResult.style.display = 'block'; //Покажем поле ввода
         }
     }
-
-    res_inp.onchange = () => {
-        if (res_inp.value != result) {
-            res_inp.style.color = 'red';
+    //Событие нажатия поля ввода результата
+    inputResult.onkeyup = () => {
+        if (inputResult.value != result) {
+            inputResult.style.color = 'firebrick';
         } else {
-            ask.style.display = 'inline-block';
-            ask.innerHTML = result;
-            res_inp.style.display ='none';
+            resBox.innerText = result;
+            inputResult.style.display = 'none'; 
+            resBox.style.display = 'block';
         }
     }
 }
